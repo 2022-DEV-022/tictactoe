345bc11f300679bbd9681f64b3ce1eae21bcfbe8
2022-DEV-022 <dev.022.2022@outlook.com>
2022-04-04 15:53:09 +0200

    feat(): Add BoardEngine protocol which define what the GameEngine need to be capable of and implement a base class that extend it.

---
+++ TicTacToe_kata/Model/GameEngine.swift
@@ -0,0 +1,61 @@
+//
+//  GameEngine.swift
+//  TicTacToe_kata
+//
+//
+
+import Foundation
+
+protocol BoardEngine {
+
+    var winnerName: String {get}
+    var gameStatus: GameStatus {get set}
+    var currentPlayer: Case {get set}
+
+    mutating func switchPlayer()
+    mutating func resetGame()
+    mutating func setCaseAt(_ index: Int) -> Case?
+}
+
+struct GameEngine: BoardEngine {
+
+    private var allowedMoves = Constants.Base.allowedMoves
+    private var gameState: GameStateProtocol
+    var gameStatus: GameStatus = GameStatus.progress
+    var currentPlayer: Case = .empty
+
+    /**
+     Constructs the winner's name
+     - returns: a String that represents the name of the winner
+     */
+    var winnerName: String {
+        return ""
+    }
+
+    init(_ gameState: GameStateProtocol = GameState()) {
+        self.gameState = gameState
+    }
+
+    mutating func setCaseAt(_ index: Int) -> Case? {
+        return nil
+    }
+
+    mutating func resetGame() {
+        allowedMoves = Constants.Base.allowedMoves
+        gameStatus = GameStatus.progress
+        currentPlayer = .empty
+        gameState = GameState()
+    }
+
+    /**
+     Switch the previous player.
+     If this is the first move, the player will be automatically a cross, then the player will switch to the opposite.
+     */
+    mutating func switchPlayer() {
+    }
+
+    // MARK: Convenients
+    private func getNextCase() -> Case {
+        return .empty
+    }
+}
