42d2bc96008dfbc96d5404b3e31af7ff5ef0ad38
2022-DEV-022 <dev.022.2022@outlook.com>
2022-04-05 09:33:28 +0200

    feat(): Add interaction between Controller and ViewModel to update the View following the user interaction

--- TicTacToe_kata/Features/Board/Controller/BoardViewController.swift
+++ TicTacToe_kata/Features/Board/Controller/BoardViewController.swift
@@ -15,12 +15,30 @@ class BoardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        boardViewModel = BoardViewModel()
+        boardViewModel = BoardViewModel(delegate: self)
     }
 
     @IBAction func didPressBox(_ sender: UIButton) {
+        boardViewModel.didSelectSquare(at: sender.tag)
     }
 
     @IBAction func didPressReset(_ sender: UIButton) {
+        resetButton.isHidden = true
+        for button in buttons {
+            button.setImage(nil, for: .normal)
+        }
+        statusLabel.text = nil
+        boardViewModel.resetGame()
+    }
+}
+
+extension BoardViewController: BoardVMDelegate {
+    func gameFinished(text: String) {
+        statusLabel.text = text
+        resetButton.isHidden = false
+    }
+
+    func setImage(at index: Int, image: String) {
+        buttons[index].setImage(UIImage(named: image), for: .normal)
     }
 }
--- TicTacToe_kata/Features/Board/ViewModel/BoardViewModel.swift
+++ TicTacToe_kata/Features/Board/ViewModel/BoardViewModel.swift
@@ -5,8 +5,50 @@
 
 import UIKit
 
-final class BoardViewModel {
+protocol BoardVMDelegate: AnyObject {
+    func gameFinished(text: String)
+    func setImage(at index: Int, image: String)
+}
 
-    init() {
+final class BoardViewModel {
+    private weak var delegate: BoardVMDelegate?
+    private var gameEngine: BoardEngine
+
+    init(gameEngine: BoardEngine = GameEngine(), delegate: BoardVMDelegate) {
+        self.gameEngine = gameEngine
+        self.delegate = delegate
+    }
+
+    func didSelectSquare(at index: Int) {
+        guard let newCase = gameEngine.setCaseAt(index) else {
+            return
+        }
+        gameEngine.switchPlayer()
+
+        delegate?.setImage(at: index, image: newCase.rawValue)
+        checkGameState()
+    }
+
+    func resetGame() {
+        gameEngine.resetGame()
+    }
+
+    /**
+     Checks game's status and call the appropriate delegate function.
+     if the game's status is a draw or a win, the delegate function gameFinished(text: String) will be called.
+     */
+    private func checkGameState() {
+
+        // in real world this should be a localized string and not a magic string
+        switch gameEngine.gameStatus {
+        case .draw:
+            delegate?.gameFinished(text: "draw")
+        case .win:
+            let text = gameEngine.winnerName + "has win"
+            delegate?.gameFinished(text: text)
+        default:
+            // game in progress
+            break
+        }
     }
 }
